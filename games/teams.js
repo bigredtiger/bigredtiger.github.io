17afa8a7bf9176c56fedd09aad3c2672156c2f33
bigredtiger <simon@bigredtiger.com>
2017-10-29 21:23:47 -0400

    Added entire site

---
+++ games/teams.js
@@ -0,0 +1,269 @@
+const nyy = {
+	team: 'NYY',
+	longName: '2009 New York Yankees',
+	color: 'black',
+	starters: [
+		{
+			name: 'Mariano Rivera',
+			ba: .2,
+			hr: 10
+		},
+		{
+			name: 'Robinson Cano',
+			ba: .305,
+			hr: 297
+		},
+		{
+			name: 'Johnny Damon',
+			ba: .284,
+			hr: 235
+		},
+		{
+			name: 'Derek Jeter',
+			ba: .310,
+			hr: 260
+		},
+		{
+			name: 'Hideki Matsui',
+			ba: .282,
+			hr: 175
+		},
+		{
+			name: 'Jorge Posada',
+			ba: .273,
+			hr: 275
+		},
+		{
+			name: 'Cody Ransom',
+			ba: .213,
+			hr: 30
+		},
+		{
+			name: 'Nick Swisher',
+			ba: .249,
+			hr: 245
+		},
+		{
+			name: 'Mark Teixeira',
+			ba: .268,
+			hr: 409
+		}
+	],
+	pitcherIndex: 0
+};
+
+const sf = {
+	team: 'SF',
+	longName: '2014 San Francisco Giants',
+	color: 'orange',
+	starters: [
+		{
+			name: 'Tim Lincecum',
+			ba: .2,
+			hr: 10
+		},
+		{
+			name: 'Buster Posey',
+			ba: .308,
+			hr: 128
+		},
+		{
+			name: 'Brandon Belt',
+			ba: .268,
+			hr: 98
+		},
+		{		
+			name: 'Joe Panik',
+			ba: .276,
+			hr: 26
+		},
+		{
+			name: 'Pablo Sandoval',
+			ba: .286,
+			hr: 121
+		},
+		{
+			name: 'Brandon Crawford',
+			ba: .249,
+			hr: 68
+		},
+		{
+			name: 'Mike Morse',
+			ba: .274,
+			hr: 105
+		},
+		{
+			name: 'Angel Pagan',
+			ba: .28,
+			hr: 64
+		},
+		{
+			name: 'Hunter Pence',
+			ba: .282,
+			hr: 217
+		}
+	],
+	pitcherIndex: 0
+};
+
+const nym = {
+	team: 'NYM',
+	longName: '1986 New York Mets',
+	color: 'blue',
+	starters: [
+		{
+			name: 'Dwight Gooden',
+			ba: .2,
+			hr: 10
+		},
+		{
+			name: 'Gary Carter',
+			ba: .262,
+			hr: 324
+		},
+		{
+			name: 'Lenny Dykstra',
+			ba: .285,
+			hr: 81
+		},
+		{
+			name: 'Mookie Wilson',
+			ba: .274,
+			hr: 67
+		},
+		{
+			name: 'Keith Hernandez',
+			ba: .296,
+			hr: 162
+		},
+		{
+			name: 'Ray Knight',
+			ba: .271,
+			hr: 84
+		},
+		{
+			name: 'Rafael Santana',
+			ba: .246,
+			hr: 13
+		},
+		{
+			name: 'Darryl Strawberry',
+			ba: .259,
+			hr: 335
+		},
+		{
+			name: 'Tim Teufel',
+			ba: .254,
+			hr: 86
+		}		
+	],
+	pitcherIndex: 0
+};
+
+const bos = {
+	team: 'Bos',
+	longName: '2004 Boston Red Sox',
+	color: 'red',
+	starters: [
+		{
+			name: 'Curt Schilling',
+			ba: .2,
+			hr: 10
+		},
+		{
+			name: 'Mark Bellhorn',
+			ba: .230,
+			hr: 69
+		},
+		{
+			name: 'Johnny Damon',
+			ba: .284,
+			hr: 235
+		},
+		{
+			name: 'Kevin Millar',
+			ba: .274,
+			hr: 170
+		},
+		{
+			name: 'Bill Mueller',
+			ba: .291,
+			hr: 85
+		},
+		{
+			name: 'David Ortiz',
+			ba: .286,
+			hr: 541
+		},
+		{
+			name: 'Manny Ramirez',
+			ba: .312,
+			hr: 555
+		},
+		{
+			name: 'Pokey Reese',
+			ba: .248,
+			hr: 44
+		},
+		{
+			name: 'Jason Varitek',
+			ba: .256,
+			hr: 193
+		}		
+	],
+	pitcherIndex: 0
+};
+
+const oak = {
+	team: 'Oak',
+	longName: '2002 Oakland Athletics',
+	color: 'lightgreen',
+	starters: [
+		{
+			name: 'Barry Zito',
+			ba: .2,
+			hr: 10
+		},
+		{
+			name: 'Eric Chavez',
+			ba: .268,
+			hr: 260
+		},
+		{
+			name: 'Jeremy Giambi',
+			ba: .263,
+			hr: 52
+		},
+		{
+			name: 'Scott Hatteberg',
+			ba: .273,
+			hr: 106
+		},
+		{
+			name: 'Ramon Hernandez',
+			ba: .263,
+			hr: 169
+		},
+		{
+			name: 'David Justice',
+			ba: .279,
+			hr: 305
+		},
+		{
+			name: 'Terrence Long',
+			ba: .269,
+			hr: 69
+		},
+		{
+			name: 'Carlos Pena',
+			ba: .232,
+			hr: 286
+		},
+		{
+			name: 'Miguel Tejada',
+			ba: .285,
+			hr: 307
+		}		
+	],
+	pitcherIndex: 0
+};
